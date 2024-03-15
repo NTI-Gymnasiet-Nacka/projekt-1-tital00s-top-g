fcb2e9472c0b1847c7ab33d07ffcc2d934c9c87a
Måns H <mans.hallander@elev.ga.ntig.se>
2024-03-15 13:17:38 +0100

    Kommentarer

--- Bugattiräknare_topG.py
+++ Bugattiräknare_topG.py
@@ -5,41 +5,49 @@ window.geometry("400x400")
 window.resizable(0, 0)
 window.title("Calculator")
 
+# Funktion för att hantera knapptryckningar
 def btn_click(item):
     global expression
     expression = expression +str(item)
     input_text.set(expression)
-    
-    
+#Function för att rensa skärmen     
 def bt_Clear():
     global expression
     expression = ""
     input_text.set("")
-
+#Function för att beräkna och visa resultat 
 def bt_equal():
     global expression
+    try:
+        result = str(eval(expression))  
+        input_text.set(result)
+    except (SyntaxError, ZeroDivisionError) as e:
+        input_text.set("Error")
+        print("syntax error")  
+    finally:
+        expression = ""
     result = str(eval(expression))
     input_text.set(result)
     expression = ""
     
 expression = ""
 
-input_text = StringVar()
+input_text = StringVar()  # variabel för att lagra texten som ska visasa på skärmen 
 
+# Skapa ramen för displayen
 input_frame = Frame(window, width=312, height=50, bd=0,highlightbackground="black", highlightcolor="black", highlightthickness=2 )
-
 input_frame.pack(side=TOP)
 
-input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT, )
-
+#skapa själva displayen
+input_field = Label(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT, )
 input_field.grid(row=0, column=0)
- 
 input_field.pack(ipady=10) 
 
+#Skapa ramen för knapparna
 btns_frame = Frame(window, width=312, height=272.5, bg="grey")
-
 btns_frame.pack()
 
+#skapa knapparna med respektive functioner och placera dem i rutnät
 clear = Button(btns_frame, text = "AC", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_Clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
  
 divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
@@ -73,24 +81,5 @@ zero = Button(btns_frame, text = "0", fg = "black", width = 21, height = 3, bd =
 point = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
  
 equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
